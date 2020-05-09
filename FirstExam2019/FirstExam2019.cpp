d26033cfd567e22ae8d8814e57a5b3e3a8635b8e
정표용 <sur2@naver.com>
2020-05-10 00:00:43 +0900

    EasyWin32

---
+++ FirstExam2019/FirstExam2019.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "tipsware.h"
+
+// 이 프로그램은 특별한 메세지를 사용하지 않는다.
+NOT_USE_MESSAGE
+
+int main(void)
+{
+	// std 라이브러리의 printf가 아님!!
+	printf(20, 5, RGB(0, 0, 255), "Hello world!");
+	
+	// 정보를 윈도우에 출력한다.
+	ShowDisplay(); 
+
+	return 0;
+}
+
+
